d1879f8db0ac52de3042b54822cb2e7a7651b70c
kneeraj825 <kneeraj825@gmail.com>
2020-02-19 12:34:44 +0530

    Added:Win and loose amount

--- gamblerSimulator.sh
+++ gamblerSimulator.sh
@@ -4,3 +4,15 @@ echo Welcome
 #constants
 STAKE_PER_DAY=100
 BET_EACH_CHANCE=1
+IS_WIN=1
+cash=100
+winCheck=$(( RANDOM%2 ))
+if (( $IS_WIN == winCheck ))
+then
+	echo "win"
+	((cash++))
+else
+	echo "loose"
+	((cash--))
+fi
+echo "$cash"
