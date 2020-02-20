8b549ed085a91fd0030ddec8e46a146111f1ec88
kneeraj825 <kneeraj825@gmail.com>
2020-02-20 14:56:26 +0530

    Added:Monthly win and loss

--- gamblerSimulator.sh
+++ gamblerSimulator.sh
@@ -9,37 +9,36 @@ total_cash_After_Month=0
 days=20
 #variables
 cash=STAKE_PER_DAY
-MAXIMUM_WIN=$(($STAKE_PER_DAY + $STAKE_PER_DAY/2))
-MAXIMUM_LOOSE=$(($STAKE_PER_DAY/2))
-
 declare -A cashCount
 #storing the  days count
 for ((  count=1; count<=days; count++))
 do
 	cash=STAKE_PER_DAY
-	echo "$count=${cashCount[$count]}"
+	MAXIMUM_WIN=$(($STAKE_PER_DAY + $STAKE_PER_DAY/2))
+	MAXIMUM_LOOSE=$(($STAKE_PER_DAY/2))
 	while (( $cash != $MAXIMUM_WIN && $cash != $MAXIMUM_LOOSE ))
 	do
 #checking the random win or loose
 		winCheck=$(( RANDOM%2 ))
 		if (( $IS_WIN == $winCheck ))
 		then
-			echo "$MAXIMUM_WIN:win"
+			echo "won by: $MAXIMUM_WIN"
 			((cash++))
 		else
-			echo "$MAXIMUM_LOOSE:loose"
+			echo "loose by: $MAXIMUM_LOOSE"
 			((cash--))
 		fi
+		echo $cash
+		Cash=$(($cash-$STAKE_PER_DAY))
 	done
-	cash=$(($STAKE_PER_DAY-$cash))
 	echo $cash
 #storing the daily winning and loosing cost in dictionary
 	cashCount[$count]=$cash
-	total_cash_After_Month=$(($(($total_cash_After_Month))+$(($cash))))
+	total_Cash_After_Month=$(($(($total_cash_After_Month))+$(($Cash))))
 done
-echo "total cash of 20 days: $total_cash_After_Month"
-echo ${cashCount[@]}
-echo ${#cashCount[@]}
+echo "total cash of 20 days: $total_Cash_After_Month"
+echo "${cashCount[@]}"
+echo "${!cashCount[@]}"
 
 
 
