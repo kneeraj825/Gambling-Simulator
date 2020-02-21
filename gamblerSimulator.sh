ee1283e4fc56506704e31d35e4acca3a99396b19
kneeraj825 <kneeraj825@gmail.com>
2020-02-21 11:08:11 +0530

    Added:Finding the luckiest and unluckiest day

--- gamblerSimulator.sh
+++ gamblerSimulator.sh
@@ -5,41 +5,48 @@ echo Welcome
 STAKE_PER_DAY=100
 BET_EACH_CHANCE=1
 IS_WIN=1
-total_cash_After_Month=0
+total_Cash_After_Month=0
 days=20
+secondCash=$STAKE_PER_DAY
 #variables
-cash=STAKE_PER_DAY
-declare -A cashCount
+SumOfCash=STAKE_PER_DAY
 #storing the  days count
 for ((  count=1; count<=days; count++))
 do
-	cash=STAKE_PER_DAY
+	cash=$STAKE_PER_DAY
 	MAXIMUM_WIN=$(($STAKE_PER_DAY + $STAKE_PER_DAY/2))
 	MAXIMUM_LOOSE=$(($STAKE_PER_DAY/2))
 	while (( $cash != $MAXIMUM_WIN && $cash != $MAXIMUM_LOOSE ))
 	do
+
 #checking the random win or loose
 		winCheck=$(( RANDOM%2 ))
 		if (( $IS_WIN == $winCheck ))
 		then
-			echo "won by: $MAXIMUM_WIN"
 			((cash++))
 		else
-			echo "loose by: $MAXIMUM_LOOSE"
 			((cash--))
 		fi
-		echo $cash
-		Cash=$(($cash-$STAKE_PER_DAY))
+		SumOfCash=$(($cash-$STAKE_PER_DAY))
 	done
-	echo $cash
-#storing the daily winning and loosing cost in dictionary
-	cashCount[$count]=$cash
-	total_Cash_After_Month=$(($(($total_cash_After_Month))+$(($Cash))))
+	SumOfCash[count]=$cash
+	total_Cash_After_Month=$(($total_Cash_After_Month+$SumOfCash))
+	total_Cash_After_Month[count]=$total_Cash_After_Month
 done
-echo "total cash of 20 days: $total_Cash_After_Month"
-echo "${cashCount[@]}"
-echo "${!cashCount[@]}"
+echo "${SumOfCash[@]}"
+echo "${!SumOfCash[@]}"
+echo "${total_Cash_After_Month[@]}"
+echo "total SumOfCash of 20 days: $total_Cash_After_Month"
+echo "Luckiest Day:"
+	for k in ${!total_Cash_After_Month[@]}
+	do
+		echo  $k '-' ${total_Cash_After_Month[$k]}
+	done | sort -rn -k3 | head -1
+		echo "UnLuckiest Day:"
+	for k in ${!total_Cash_After_Month[@]}
+	do
+		echo  $k '-' ${total_Cash_After_Month[$k]}
+	done | sort -rn -k3 | tail -1
 
-
-
+echo "total SumOfCash of 20 days: $total_Cash_After_Month"
 
